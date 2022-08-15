ae1b8673189c0ffa863fb2a673eb8982ac1f65be
Dardan Gjoka <dardan.gjoka@sqa-consulting.com>
2022-08-15 15:37:28 +0200

    Added the Driver silngleton and the ConfigurationReader under the Util packet

--- src/test/java/com/sqa/academy/utils/Driver.java
+++ src/test/java/com/sqa/academy/utils/Driver.java
@@ -1,4 +1,48 @@
 package com.sqa.academy.utils;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 public class Driver {
+    private static WebDriver driver;
+    private Driver(){
+    }
+
+
+    public static WebDriver getDriver(){
+        String browser=ConfigurationReader.get("browser");
+        switch (browser){
+            case "chrome":
+                WebDriverManager.chromedriver().setup();
+                driver=new ChromeDriver();
+                // driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+                break;
+            case "fireFox":
+                WebDriverManager.firefoxdriver().setup();
+                driver=new FirefoxDriver();
+                //driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+                break;
+
+            case "edge":
+                WebDriverManager.edgedriver().setup();
+                driver=new EdgeDriver();
+                //driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+                break;
+
+
+
+        }
+
+        return driver;
+
+    }
+    public static void  close(){
+        if(driver!=null) {
+            driver.quit();
+            driver = null;
+        }
+    }
 }
