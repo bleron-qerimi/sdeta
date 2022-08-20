0b594af3a61812c96854367b9df431195cb91038
LabinotGjoka <labinot.gjoka@sqa-consulting.com>
2022-08-20 17:25:15 +0200

    Added on PasePage the PageFactory, and a Locatro for the leftnavBar, Added a Hooks class with Befor and After Method, Created a testng.xml(with my test cases configs), Added My test Case As a class TheUserShouldSeeAllThePetsInASpecificOrder.

--- src/test/java/com/sqa/academy/pages/BasePage.java
+++ src/test/java/com/sqa/academy/pages/BasePage.java
@@ -1,4 +1,16 @@
 package com.sqa.academy.pages;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import java.util.List;
+
 public class BasePage {
+    public BasePage(WebDriver driver){
+        PageFactory.initElements(driver, this);
+    }
+    @FindBy(xpath = "//div[@id='SidebarContent']/a")
+    public List<WebElement> leftSideMenu;
 }
