c16c97b84e88617891701b44e04d8c708440a367
Jasmina Zukancic <jasmina.zukancic@sqa-consulting.com>
2022-08-23 10:14:17 +0200

    new computer should be created

--- src/test/java/com/sqa/academy/actions/SDETA43/NewComputerCreated.java
+++ src/test/java/com/sqa/academy/actions/SDETA43/NewComputerCreated.java
@@ -2,16 +2,20 @@ package com.sqa.academy.actions.SDETA43;
 
 import com.sqa.academy.actions.Hooks;
 import com.sqa.academy.utils.ConfigurationReader;
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class FillTheForm extends Hooks {
-    //user should be able to fill the form
+import java.util.Objects;
+
+public class NewComputerCreated extends Hooks {
+    //new computer should be created
     @Test
-            public static void FillForm(){
-    String url2 = ConfigurationReader.get("url2");
+    public static void ComputerCreated(){
+        String url2= ConfigurationReader.get("url2");
         driver.get(url2);
         driver.findElement(By.id("name")).sendKeys("HP");
         driver.findElement(By.id("introduced")).sendKeys("2006-09-17");
@@ -19,5 +23,10 @@ public class FillTheForm extends Hooks {
         WebElement selectCompany = driver.findElement(By.id("company"));
         Select select = new Select(selectCompany);
         select.selectByVisibleText("Hewlett-Packard");
+        driver.findElement(By.xpath("//input[@type='submit' and @value='Create this computer']")).click();
+        String alertMsg= driver.findElement(By.xpath("/html/body/section/div[1]")).getText();
+        if(!Objects.equals(alertMsg, "Done ! Computer HP has been created")){
+            Assert.fail();
+        }
     }
 }
