b842b5dfbec06c396e172e241a2a2765c23c32ff
Jasmina Zukancic <jasmina.zukancic@sqa-consulting.com>
2022-08-23 09:43:48 +0200

    user should be able to fill the form...

---
+++ src/test/java/com/sqa/academy/actions/SDETA43/FillTheForm.java
@@ -0,0 +1,24 @@
+package com.sqa.academy.actions.SDETA43;
+
+import com.sqa.academy.actions.Hooks;
+import com.sqa.academy.utils.ConfigurationReader;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.annotations.Test;
+
+public class FillTheForm extends Hooks {
+    //user should be able to fill the form
+    @Test
+            public static void FillForm(){
+    String url2 = ConfigurationReader.get("url2");
+        driver.get(url2);
+        driver.findElement(By.id("name")).sendKeys("HP");
+        driver.findElement(By.id("introduced")).sendKeys("2006-09-17");
+        driver.findElement(By.id("discontinued")).sendKeys("2022-03-22");
+        WebElement selectCompany = driver.findElement(By.id("company"));
+        Select select = new Select(selectCompany);
+        select.selectByVisibleText("Hewlett-Packard");
+        driver.findElement(By.xpath("//input[@type='submit' and @value='Create this computer']")).click();
+    }
+}
