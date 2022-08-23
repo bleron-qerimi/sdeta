9e3993d95823704be86773d4b850d559975f0acc
Dardan Gjoka <dardan.gjoka@sqa-consulting.com>
2022-08-23 10:25:58 +0200

    Refactored the pages, added two new packages for each of the envirenments that we will be Testing

--- src/test/java/com/sqa/academy/pages/jPetStore/HomePage.java
+++ src/test/java/com/sqa/academy/pages/jPetStore/HomePage.java
@@ -1,5 +1,6 @@
-package com.sqa.academy.pages;
+package com.sqa.academy.pages.jPetStore;
 
+import com.sqa.academy.pages.jPetStore.AbstractBasePage;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
