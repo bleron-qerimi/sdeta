572ff149ebca06a764377c7a36d470896371c2b9
Anzotike Bilalli <anzotike.bilalli@sqa-consulting.com>
2022-08-24 14:47:02 +0200

    Constructed the code on the orders of the columns and pagination buttons for navigation

--- src/test/java/com/sqa/academy/actions/SDETA42/EditsInEditView.java
+++ src/test/java/com/sqa/academy/actions/SDETA42/EditsInEditView.java
@@ -15,10 +15,11 @@ public class EditsInEditView extends Hooks{
             //go to the enviornment
             GeneralSteps.gotToEnv("Computer");
             ComputerDatabaseElements databaseElements=new ComputerDatabaseElements(driver);
-
+            //edit the fields
             databaseElements.clickCmp();
             databaseElements.addName("Dell");
             databaseElements.addIntroduced("2001-01-01");
+            //save the edits
             databaseElements.svBtn();
             Assert.assertEquals(databaseElements.clickCmp.getText(),"Dell");
 
